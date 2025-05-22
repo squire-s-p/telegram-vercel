5165278d9f54c941f3e3781a87284b6519d3c702
squire-s-p <36736776+squire-s-p@users.noreply.github.com>
2025-05-22 19:00:21 +0300

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
   const CHAT_IDS = process.env.CHAT_IDS.split(",");
 
